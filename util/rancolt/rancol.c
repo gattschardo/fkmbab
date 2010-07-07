80f41d48bbe908042abb87623fcdc1720fb90708
Richard Molitor <gattschardo@gmail.com>
2010-07-07 13:14:15 +0200

    fixed some bugs in rancol

--- util/rancolt/rancol.c
+++ util/rancolt/rancol.c
@@ -5,12 +5,17 @@
 
 int main(void)
 {
+	int col;
+	char str[8];
+
 	srand(time(0));
 
-	int col = rand() % 0x1000000;
+	col = rand() % 0x1000000;
+	col = col < 0 ? -col : col;
 
-	char str[8];
-	sprintf(str, "#%06x", col);
+	snprintf(str, 8, "#%06x", col);
 
 	execlp("/usr/bin/urxvt", "urxvt", "-fg", str, "-tint", str, 0);
+
+	return 1;
 }
